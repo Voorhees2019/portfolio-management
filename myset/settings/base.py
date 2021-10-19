9853152d7abe9c037f1dcccc14e5009b5602a9d4
Artur Petryk <arturpetryk@planeks.net>
2021-10-19 17:27:19 +0300

    hotfix associate email while login via LinkedIn

--- myset/settings/base.py
+++ myset/settings/base.py
@@ -214,6 +214,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.user.create_user',
     'apps.accounts.pipeline.user.confirm_email',  # email is already verified by LinkedIn. Set `email_confirmed` to True
     'social_core.pipeline.social_auth.associate_user',
